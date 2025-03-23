4a1b696f81d6bb8877f8c8277edf723876b9fe0c
aztec-dev <aztech.dev1@gmail.com>
2025-03-23 21:56:09 +1000

    added functional name test

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ Author: Azariah Pundari
 """
 
 from flask import Flask
+from markupsafe import escape
 
-app = Flask(__name__)
+app = Flask(__name__)  # Creates an instance of flask
 
 @app.route("/")
 def weather_forecast():
     return "<h1>Weather Forecast</h1>"
+
+@app.route("/<name>")
+def hello(name):
+    return f"<h1>Hello {escape(name)}"
