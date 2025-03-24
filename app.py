3ca539c6f8697b7c498fa4bfedefc4b2d3129117
aztec-dev <aztech.dev1@gmail.com>
2025-03-24 10:51:12 +1000

    feat: hidden api call

--- app.py
+++ app.py
@@ -4,22 +4,27 @@ Date: 22/03/2025
 Author: Azariah Pundari
 """
 
+import os
 from flask import Flask, jsonify
 from flask import render_template
+from dotenv import load_dotenv
 import requests
+
+# Load environment variables
+load_dotenv()
 
 app = Flask(__name__)  # Creates an instance of flask
 
 @app.route("/weather_forecast", methods=['GET'])
 def get_forecast():
     # Testing API get
-    api_key = "310fb733be43977ce18fd565acafd2d0"
+    api_key = os.getenv("API_KEY")
     params = ["-11.8092", "51.509865"]
-    api_url = f"https://api.openweathermap.org/data/3.0/onecall/overview?lat={params[0]}&lon={params[1]}&appid={api_key}"
+    api_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={params[0]}&lon={params[1]}&appid={api_key}"
 
     # Make a request
     response = requests.get(api_url)
-    if response.status_code == 401:
+    if response.status_code == 200:
         weather_data = response.json()
         return jsonify(weather_data)  # Return JSON to the user
     else:
